9ea3526bd3835a453fdc1da538641f3ac7c041a1
mohammadoeko <mohammadoeko@gmail.com>
2020-10-17 01:47:07 +0700

    Read Ok

--- app/Http/Controllers/Admin/KategoriController.php
+++ app/Http/Controllers/Admin/KategoriController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Kategori;
 
 class KategoriController extends Controller
 {
@@ -15,7 +16,8 @@ class KategoriController extends Controller
     public function index()
     {
         //
-        return view('admin.kategori.index');
+        $data=Kategori::all();
+        return view('admin.kategori.index', compact('data'));
     }
 
     /**
